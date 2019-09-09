9ad97c1d63ab8a6242d1a492178b4ceab17be4e8
swon3210 <swon3210@gmail.com>
2019-09-09 13:50:49 +0900

    news frontend initialized

---
+++ layouts/default.vue
@@ -0,0 +1,105 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      v-model="drawerRight"
+      app
+      clipped
+      right
+    >
+      <v-list dense>
+        <v-list-item @click.stop="right = !right">
+          <v-list-item-action>
+            <v-icon>mdi-exit-to-app</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Open Temporary Drawer</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      clipped-right
+      color="blue-grey"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Toolbar</v-toolbar-title>
+      <div class="flex-grow-1"></div>
+      <v-app-bar-nav-icon @click.stop="drawerRight = !drawerRight"></v-app-bar-nav-icon>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <v-list-item 
+          v-for="item in items"
+          :key="item.id"
+          :to="item.to"
+        >
+          <v-list-item-action>
+            <v-icon>{{item.icon}}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{item.title}}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-navigation-drawer
+      v-model="left"
+      fixed
+      temporary
+    ></v-navigation-drawer>
+
+    <v-content>
+      <nuxt />
+    </v-content>
+
+    <v-navigation-drawer
+      v-model="right"
+      fixed
+      right
+      temporary
+    ></v-navigation-drawer>
+
+    <!-- <v-footer
+      app
+      color="blue-grey"
+      class="white--text"
+    >
+      <span>Vuetify</span>
+      <div class="flex-grow-1"></div>
+      <span>&copy; 2019</span>
+    </v-footer> -->
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: '뉴스',
+          to: '/'
+        },
+        {
+          icon: 'mdi-apps',
+          title: '소개',
+          to: '/intro'
+        },
+      ],
+      drawer: null,
+      drawerRight: null,
+      right: false,
+      left: false,
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-container fill-height>
+    <v-row
+      class="flex-child ma-0"
+    >
+      <v-col
+        class="d-flex"
+        cols="12"
+      >
+        <v-row>
+          <v-col cols="12">
+            <v-sheet
+              class="d-flex"
+              color="white lighten-3"
+              height="1000"
+              :elevation="2"
+            >
+              
+            </v-sheet>
+          </v-col>
+          <!-- <v-col cols="6">
+            <v-sheet
+              class="d-flex"
+              color="green lighten-3"
+              height="200"
+            >
+            </v-sheet>
+          </v-col>
+          <v-col cols="6">
+            <v-sheet
+              class="d-flex"
+              color="yellow lighten-3"
+              height="200"
+            >
+            </v-sheet>
+          </v-col> -->
+          <v-col>
+            <div class="text-center">
+              <v-pagination
+                v-model="page"
+                :length="6"
+              ></v-pagination>
+            </div>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+    
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      page: 1,
+    }
+  },
+  components: {
+
+  }
+}
+</script>
