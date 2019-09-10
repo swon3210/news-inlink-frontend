f13074227b8cc8e437d26eddd8025c9973182516
swon3210 <swon3210@gmail.com>
2019-09-10 18:05:10 +0900

    example text and image inserted

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,9 +12,30 @@
             <v-icon>mdi-exit-to-app</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>Open Temporary Drawer</v-list-item-title>
+            <v-list-item-title>신문사 바꾸기</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
+      </v-list>
+      <v-list>
+        <v-list-item-group
+          v-model="model2"
+          v-for="(item, i) in items3"
+          :key="i"
+        >
+            <v-divider
+              :key="`divider-${i}`"
+            ></v-divider>
+
+            <v-list-item
+              :key="`item-${i}`"
+              :value="item"
+              active-class="deep-purple--text text--accent-4"
+            >
+              <v-list-item-content>
+                <v-list-item-title v-text="item"></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+        </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
 
@@ -25,7 +46,7 @@
       dark
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>Toolbar</v-toolbar-title>
+      <v-toolbar-title>네이버 뉴스</v-toolbar-title>
       <div class="flex-grow-1"></div>
       <v-app-bar-nav-icon @click.stop="drawerRight = !drawerRight"></v-app-bar-nav-icon>
     </v-app-bar>
@@ -65,7 +86,23 @@
       fixed
       right
       temporary
-    ></v-navigation-drawer>
+    >
+      <v-list>
+        <v-list-item-group v-model="model">
+          <v-list-item
+            v-for="(item, i) in items2"
+            :key="i"
+          >
+            <v-list-item-icon>
+              <v-icon v-text="item.icon"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.text"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
 
     <!-- <v-footer
       app
@@ -95,8 +132,35 @@ export default {
           to: '/intro'
         },
       ],
-      drawer: null,
-      drawerRight: null,
+      model: 1,
+      items2: [
+        {
+          icon: 'mdi-inbox',
+          text: 'A',
+        },
+        {
+          icon: 'mdi-star',
+          text: 'B',
+        },
+        {
+          icon: 'mdi-send',
+          text: 'C',
+        },
+        {
+          icon: 'mdi-email-open',
+          text: 'D',
+        },
+      ],
+      items3: [
+        '최근 기사 1',
+        '최근 기사 2',
+        '최근 기사 3',
+        '최근 기사 4',
+        '최근 기사 5',
+      ],
+      model2: ['Carrots'],
+      drawer: true,
+      drawerRight: true,
       right: false,
       left: false,
     }
